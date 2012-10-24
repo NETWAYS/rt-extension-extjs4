94d33337d78d6f3468fa15e874754e5c0d12681c
Marius Hein <marius.hein@netways.de>
2012-10-24 11:49:32 +0200

    Initial commit of Sencha ExtJS 4 framework for rt4

---
+++ lib/RTx/ExtJS4.pm
@@ -0,0 +1,36 @@
+package RTx::ExtJS4;
+
+use strict;
+use version;
+
+our $VERSION="1.0.0";
+
+1;
+=pod
+
+=head1 NAME
+
+RTx::ExtJS4
+
+=head1 VERSION
+
+version 1.0.0
+
+=head1 AUTHOR
+
+Marius Hein <marius.hein@netways.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by NETWAYS GmbH <info@netways.de>
+
+This is free software, licensed under:
+    GPL Version 2, June 1991
+
+ExtJS is licensed unter GPLv3. This Module is only
+an installer coating and has nothing to do with
+any ExtJS application itself.
+
+=cut
+
+__END__
