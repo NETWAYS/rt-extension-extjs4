4afdc8e64855b7598970b0d825beac784921828e
Marius Hein <marius.hein@netways.de>
2015-11-12 17:23:48 +0100

    Remove link to actitime task window
    This window does not exist anymore.

--- html/NoAuth/RTx-Actitime/js/view/DetailButton.js
+++ html/NoAuth/RTx-Actitime/js/view/DetailButton.js
@@ -1,5 +1,5 @@
 Ext.define("Actitime.view.DetailButton", {
-    extend: "Ext.button.Split",
+    extend: "Ext.button.Button",
     alias: ["widget.task-detail"],
     requires: [
         "Actitime.util.ActualWindow",
@@ -19,20 +19,6 @@ Ext.define("Actitime.view.DetailButton", {
             Actitime.util.Format.actuals(this.task.get("sum"))
         );
         
-        this.menu = Ext.create("Ext.menu.Menu", {
-            items: [{
-                text: "Show actuals",
-                handler: this.onShowActuals,
-                scope: this,
-                iconCls: "actitime-icon-user"
-            }, {
-                text: "Show actitime",
-                handler: this.onShowActitime,
-                scope: this,
-                iconCls: "actitime-icon-bookmark"
-            }]
-        });
-        
         this.on("click", this.onShowActuals, this);
         
         this.callParent();
@@ -43,24 +29,5 @@ Ext.define("Actitime.view.DetailButton", {
         this.wnd.alignTo(this.getEl(), 'tr?');
         this.wnd.update(this.task);
         this.wnd.show();
-    },
-    
-    onShowActitime: function() {
-        var config = Actitime.util.Config;
-        var taskid = this.task.get("taskid");
-        var url = "{0}/tasks/task_details.do?taskId={1}";
-        var st = this.statics();
-        
-        url = Ext.String.format(url, config.get("actitimeurl"), taskid);
-        
-        if (!st.wnd) {
-            st.wnd = window.open();
-        } else if (st.wnd.closed === true) {
-            st.wnd = window.open();
-        }
-        
-        st.wnd.location = url;
-        st.wnd.focus();
-        
     }
 });
