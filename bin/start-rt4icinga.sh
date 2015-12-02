cb7cad078020ae26428a2358ac9e31d7127a4776
Marius Hein <marius.hein@netways.de>
2015-12-02 10:10:14 +0100

    Add missing restart argument

--- bin/start-rt4icinga.sh
+++ bin/start-rt4icinga.sh
@@ -3,6 +3,7 @@ docker run \
   --name=rt4icinga \
   --hostname=rt.icinga.org \
   --privileged=true \
+  --restart=always \
   -d \
   -p 25:25 \
   -p 80:80 \
--- bin/start-rt4netways.sh
+++ bin/start-rt4netways.sh
@@ -3,6 +3,7 @@ docker run \
   --name=rt4netways \
   --hostname=rt.netways.de \
   --privileged=true \
+  --restart=always \
   -d \
   -p 25:25 \
   -p 80:80 \
