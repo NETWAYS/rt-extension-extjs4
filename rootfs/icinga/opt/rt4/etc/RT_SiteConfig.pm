26a529dca306fef7538cc595d805eecd2bfaa562
Michael Friedrich <michael.friedrich@netways.de>
2017-10-20 13:24:51 +0200

    Fix RTAddressRegexp for Icinga RT

--- rootfs/icinga/opt/rt4/etc/RT_SiteConfig.pm
+++ rootfs/icinga/opt/rt4/etc/RT_SiteConfig.pm
@@ -32,7 +32,7 @@ Set($SuppressInlineTextFiles, 1);
 Set($ParseNewMessageForTicketCcs, 1);
 Set($ShowHistory, 'delay');
 
-Set($RTAddressRegexp, '^(info|xrt|rt|events|shop|partner|support|sales|automail|infrastructure)\@(rt\.)?icinga\.org$');
+Set($RTAddressRegexp, '^(info|xrt|rt|events|shop|partner|support|sales|automail|infrastructure)\@(rt\.)?icinga\.com$');
 Set($CorrespondAddress , 'rt@icinga.com');
 Set($CommentAddress , 'xrt@icinga.com');
 Set($SetOutgoingMailFrom, 1);
