029f358c66de4eae33bd185bd49d05fbf38eb4ef
Johannes Meyer <johannes.meyer@netways.de>
2018-03-15 13:15:25 +0100

    Netways.pm: Update perldoc format and generate README from it

--- lib/RT/Extension/Netways.pm
+++ lib/RT/Extension/Netways.pm
@@ -1,9 +1,8 @@
 package RT::Extension::Netways;
 
+use 5.010;
 use strict;
 use warnings;
-
-use 5.010;
 
 our $VERSION = '1.1.0';
 
@@ -15,9 +14,47 @@ RT-Extension-Netways - Changes on the RT system for the NETWAYS workflow
 
 =head1 DESCRIPTION
 
-Special features for NETWAYS GmbH workflow.
+Some special features for the NETWAYS GmbH.
 
 https://www.netways.de
+
+These features are the following:
+
+=over
+
+=item * Custom header for tickets [Configurable]
+
+=item * Register ticket authors as AdminCc instead of Requestor [Configurable]
+
+=item * Read-Only boxes under a ticket's basics to quickly copy and share details
+
+=item * Quick removal of poeple registered as AdminCc or Requestor
+
+=item * Quick assign of people as AdminCc or Owner [Configurable]
+
+=item * Switched positions of ticket people and reminders
+
+=item * Safety measures when merging tickets (aka A modal confirmation dialog)
+
+=item * Empty search result boxes are automatically hidden (e.g. on "RT at a glance") [Configurable]
+
+=item * Additional CSS classes for search result boxes (e.g. on "RT at a glance")
+
+=over
+
+=item netways-tickets-default (If no other of the following applies)
+
+=item netways-tickets-request
+
+=item netways-tickets-watched
+
+=item netways-tickets-nobody
+
+=item netways-tickets-due
+
+=back
+
+=back
 
 =head1 RT VERSION
 
@@ -49,9 +86,49 @@ Add this line:
 
 =back
 
+=head1 CONFIGURATION
+
+=head2 C<$Netways_EnableTagFormat>
+
+If enabled, applies the custom header for tickets.
+
+=head2 C<$Netways_UserRequestorGroup>
+
+The group of which authors must be a member to get registered as AdminCc instead of Requestor.
+
+=head2 C<$Netways_EnableQuickAssign>
+
+If enabled, shows the form controls to quickly assign people as AdminCc or Owner.
+
+=head2 C<$Netways_QuickAssignGroup>
+
+Which group's members should be choosable when showing the quick assign form controls.
+
+=head2 C<$Netways_ShowSearchOnlyWithResults>
+
+If enabled, hides empty search result boxes automatically. (e.g. on "RT at a glance")
+
+=head2 Example
+
+=over
+
+=item C<# Enables everything provided by RT::Extension::Netways>
+
+=item C<Set($Netways_EnableTagFormat, 1);>
+
+=item C<Set($Netways_UserRequestorGroup, 'NETWAYS');>
+
+=item C<Set($Netways_EnableQuickAssign, 1);>
+
+=item C<Set($Netways_QuickAssignGroup, 'NETWAYS');>
+
+=item C<Set($Netways_ShowSearchOnlyWithResults, 1);>
+
+=back
+
 =head1 AUTHOR
 
-NETWAYS GmbH <lt>info@netways.de<gt>
+NETWAYS GmbH <lt>support@netways.de<gt>
 
 =for html <p>All bugs should be reported via email to <a
 href="mailto:bug-RT-Extension-Netways@rt.cpan.org">bug-RT-Extension-Netways@rt.cpan.org</a>
