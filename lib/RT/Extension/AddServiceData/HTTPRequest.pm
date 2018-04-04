28926b4a6b849fcebfb4f3b47cbf53a154202259
Michael Friedrich <michael.friedrich@netways.de>
2018-04-04 20:10:37 +0200

    Squashed 'vendor/rt-extension-addservicedata/' content from commit 91069b63c
    git-subtree-dir: vendor/rt-extension-addservicedata
    git-subtree-split: 91069b63c9b02ca9335ffe69968a0608add32c03

---
+++ lib/RT/Extension/AddServiceData/HTTPRequest.pm
@@ -0,0 +1,88 @@
+package RT::Extension::AddServiceData::HTTPRequest;
+
+use strict;
+
+use Data::Dumper;
+
+use LWP::UserAgent;
+
+use HTTP::Request;
+
+our $VERSION = '0.9.0';
+
+require RT::Extension::AddServiceData::GenericRequest;
+
+
+use base qw (RT::Extension::AddServiceData::GenericRequest);
+
+sub new {
+
+	my $type = shift;
+
+	my $class = (ref $type || $type);
+
+	my $config = {
+		'agent' => __PACKAGE__. '/'. $VERSION,
+		@_
+	};
+
+	my $self = $class->SUPER::new(%{ $config });
+
+	$self->{'request_done'} = 0;
+
+	$self->{'ua'} = LWP::UserAgent->new(
+		agent => $self->getItem('agent')
+	);
+
+	return bless $self, $class;
+}
+
+sub getRequest {
+	my $self = shift;
+	my $req = HTTP::Request->new(GET => $self->getItem('uri'));
+
+	if ($self->getItem('user') && $self->getItem('pass')) {
+		$req->authorization_basic($self->getItem('user'), $self->getItem('pass'));
+	}
+
+	return $req;
+}
+
+sub doRequest {
+
+	my $self = shift;
+
+	use Data::Dumper;
+
+	my $req = $self->getRequest();
+
+	$self->{'response'} = $self->{'ua'}->request($req);
+
+	if ($self->{'response'}->is_success) {
+		$self->{'content'} = $self->{'response'}->content;
+	}
+	else {
+		push @{ $self->{'errors'} }, $self->{'response'}->status_line;
+	}
+
+	$self->{'request_done'} = 1;
+
+}
+
+sub getRequestData {
+
+	my $self = shift;
+
+	unless ($self->{'request_done'}) {
+		$self->doRequest();
+	}
+
+	if (scalar @{ $self->{'errors'} }) {
+		return undef;
+	}
+
+	return $self->{'content'};
+
+}
+
+1;
