a4d5c0218f739228f0b659305910a74a299d48e4
Marius Hein <mhein@itsocks.de>
2018-04-04 13:10:51 +0200

    NETWAYS: Change configuration

--- rootfs/netways/opt/rt4/etc/RT_SiteConfig.d/80-rtx-netways.pm
+++ rootfs/netways/opt/rt4/etc/RT_SiteConfig.d/80-rtx-netways.pm
@@ -1,9 +1,9 @@
-Plugin('RTx::NETWAYS');
+Plugin('RT::Extension::Netways');
 
-Set($RTx_NETWAYS_EnableQuickAssign, 1);
-Set($RTx_NETWAYS_QuickAssignGroup, 'NETWAYS');
+Set($Netways_EnableQuickAssign, 1);
+Set($Netways_QuickAssignGroup, 'NETWAYS');
 
-Set($RTx_NETWAYS_UserRequestorGroup, 'NETWAYS');
-Set($RTx_NETWAYS_EnableTagFormat, 1);
+Set($Netways_UserRequestorGroup, 'NETWAYS');
+Set($Netways_EnableTagFormat, 1);
 
-Set($RTx_NETWAYS_ShowSearchOnlyWithResults, 0);
+Set($Netways_ShowSearchOnlyWithResults, 0);
