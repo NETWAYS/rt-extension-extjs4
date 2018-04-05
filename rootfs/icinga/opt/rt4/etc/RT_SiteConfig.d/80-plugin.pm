2e47752f6d20b5038e04eabff3c7fb546a37e453
Marius Hein <mhein@itsocks.de>
2018-04-05 14:16:00 +0200

    Icinga: Remove ExtJS plugin

--- rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/80-plugin.pm
+++ rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/80-plugin.pm
@@ -1,6 +1,5 @@
 Plugin('RT::Extension::ExtractCustomFieldValues');
 Plugin('RTx::CreateLinkedTickets');
-Plugin('RTx::ExtJS4'); # TODO: Remove me after CreateLinkedTickets, Netways, DBCustomField are fully merged.
 Plugin('RT::Extension::QueueCategories');
 Plugin('RT::Action::ChangeOwner');
 Plugin('RT::Action::SetOwner');
