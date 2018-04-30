53dc0a28eedaa20e05771bf53bdf11f2954ffdc9
Tim Albert <Tim.Albert@netways.de>
2018-04-30 10:28:24 +0200

    [557477]better safe than sorry

--- rootfs/netways/opt/rt4/etc/RT_SiteConfig.d/20-mail.pm
+++ rootfs/netways/opt/rt4/etc/RT_SiteConfig.d/20-mail.pm
@@ -1,4 +1,4 @@
-Set($RTAddressRegexp, '^(nobody\@web\.|.+\@rt\.|(marketing|nws|service|info|office|abuse|billing|changes|einkauf|events|fax|hardware|hostmaster|reports|.*support|sales|shop|sun|tradoria|tickets?|grafik|telekom|hq|jobs?|schulungsanmeldung|devopsdays\.attendee)\@)netways\.de$');
+Set($RTAddressRegexp, '^(nobody\@web\.|.+\@rt\.|(starface|marketing|nws|service|info|office|abuse|billing|changes|einkauf|events|fax|hardware|hostmaster|reports|.*support|sales|shop|sun|tradoria|tickets?|grafik|telekom|hq|jobs?|schulungsanmeldung|devopsdays\.attendee)\@)netways\.de$');
 Set($CorrespondAddress , 'rt@rt.netways.de');
 Set($CommentAddress , 'xrt@rt.netways.de');
 Set($SetOutgoingMailFrom, 1);
