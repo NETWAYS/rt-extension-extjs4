a6d73d567712cabedd692a5883db5bf839c1f082
Marius Hein <mhein@itsocks.de>
2018-12-18 16:22:44 +0100

    Remove RT::Action::SetOwner for ICINGA
    refs #31

--- rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/80-plugin.pm
+++ rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/80-plugin.pm
@@ -2,7 +2,6 @@ Plugin('RT::Extension::ExtractCustomFieldValues');
 Plugin('RT::Extension::CreateLinkedTickets');
 Plugin('RT::Extension::QueueCategories');
 Plugin('RT::Action::ChangeOwner');
-Plugin('RT::Action::SetOwner');
 Plugin('RT::Extension::DBCustomField');
 Plugin('RT::Extension::HistoryComponent');
 Plugin('RT::Extension::UserSearch');
