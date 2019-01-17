565b5f298db9cac3e64f14903883a0aa5aa7d633
Blerim Sheqa <blerim.sheqa@icinga.com>
2019-01-17 15:41:34 +0100

    Add billing queue

--- rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/20-mail.pm
+++ rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/20-mail.pm
@@ -1,4 +1,4 @@
-Set($RTAddressRegexp, '^(info|xrt|rt|events|shop|partner|support|sales|automail|infrastructure)\@(rt\.)?icinga\.com$');
+Set($RTAddressRegexp, '^(info|xrt|rt|events|shop|partner|support|sales|automail|infrastructure|billing)\@(rt\.)?icinga\.com$');
 Set($CorrespondAddress , 'rt@icinga.com');
 Set($CommentAddress , 'xrt@icinga.com');
 Set($SetOutgoingMailFrom, 1);
