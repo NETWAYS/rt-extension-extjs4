f40727652a470a4f5ec12ff455f647da31993ba0
Marius Hein <mhein@itsocks.de>
2019-05-22 13:30:15 +0200

    Icinga: CustomField PartnerCustomer introduced

--- rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/80-rtx-dbcustomfield.pm
+++ rootfs/icinga/opt/rt4/etc/RT_SiteConfig.d/80-rtx-dbcustomfield.pm
@@ -10,7 +10,13 @@ Set($DBCustomField_Connections, {
 		'username'	=> 'icinga-rt',
 		'password'	=> 'rm9QsrnRf8ZRbH936IoW',
 		'autoconnect'	=> 1
-	}
+	},
+
+	'partnerportal' => {
+        'dsn' => 'DBI:mysql:database=partnerportal;host=mysql.icinga.netways.de;port=3306;mysql_enable_utf8=1',
+        'username' => 'rt4_partnerportal',
+        'password' => '3m3xEuC3aqCRQAuxfpko'
+    }
 });
 
 # Queries
@@ -61,10 +67,45 @@ Set ($DBCustomField_Queries, {
 		# columns here encapsulate their name with curly braces. The default is just `{field_value}'.
 		# HTML support: Yes, but try to avoid manipulating the layout too much (e.g. with block elements)
 		'display_value_tpl' => '{shortname} (<i>{field_value}</i>)'
-	}
+	},
+
+	'partner_customer' => {
+        'connection' => 'partnerportal',
+        'suggestions' => q{
+        	SELECT
+        	pc.id as field_value,
+        	pc.partner_id,
+        	pc.name AS customer_name,
+        	pp.name AS partner_name
+        	FROM portal_customer pc
+        	LEFT JOIN portal_partner pp ON pc.partner_id = pp.id
+			WHERE pc.archived = 0 AND (
+				pc.id LIKE ? OR pp.id LIKE ? OR pc.name LIKE ? OR pp.name LIKE ?
+			)
+        	ORDER BY pp.name, pc.name
+        },
+        'suggestions_tpl' => q{
+            <div>
+                <strong>{customer_name} <em>#{field_value}</em></strong>
+                <div>from {partner_name} <em>#{partner_id}</em></div>
+            </div>
+        },
+        'display_value' => q{
+            SELECT
+        	pc.id as field_value,
+        	pc.partner_id,
+        	pc.name AS customer_name,
+        	pp.name AS partner_name
+        	FROM portal_customer pc
+        	LEFT JOIN portal_partner pp ON pc.partner_id = pp.id
+			WHERE pc.id = ?
+        },
+        'display_value_tpl' => '{customer_name} <i>#{field_value}</i> from {partner_name} <i>#{partner_id}</i>'
+    }
 });
 
 # Query to CF mapping
 Set($DBCustomField_Fields, {
-	'Client' => 'companies'
+	'Client'          => 'companies',
+	'PartnerCustomer' => 'partner_customer'
 });
