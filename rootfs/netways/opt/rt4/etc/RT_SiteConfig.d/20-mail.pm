2d5b731128329a3a3af63d42425d422d99cc63d0
Marius Hein <mhein@itsocks.de>
2019-09-19 14:09:38 +0200

    Add tool mail adresses to RTAddressRegexp
    fixes #58
    fixes #59

--- rootfs/netways/opt/rt4/etc/RT_SiteConfig.d/20-mail.pm
+++ rootfs/netways/opt/rt4/etc/RT_SiteConfig.d/20-mail.pm
@@ -1,4 +1,4 @@
-Set($RTAddressRegexp, '^(nobody\@web\.|.+\@rt\.|(starface|marketing|nws|service|info|office|abuse|billing|changes|einkauf|events|fax|hardware|hostmaster|reports|.*support|sales|shop|sun|tradoria|tickets?|grafik|telekom|hq|jobs?|schulungsanmeldung|devopsdays\.attendee|cfp|conference|datenschutz|privacy)\@)netways\.de$');
+Set($RTAddressRegexp, '^(nobody\@web\.|.+\@rt\.|(starface|marketing|nws|service|info|office|abuse|billing|changes|einkauf|events|fax|hardware|hostmaster|reports|.*support|sales|shop|sun|tradoria|tickets?|grafik|telekom|hq|jobs?|schulungsanmeldung|devopsdays\.attendee|cfp|conference|datenschutz|privacy|MAILER-DAEMON|wordpress)\@)netways\.de$');
 Set($CorrespondAddress , 'rt@rt.netways.de');
 Set($CommentAddress , 'xrt@rt.netways.de');
 Set($SetOutgoingMailFrom, 1);
